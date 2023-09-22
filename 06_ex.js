2082165d9f65734e7dfcf03db4aeb400306a7f35
nicollasTavares <nicollascursos@gmail.com>
2023-09-21 21:48:46 -0300

    att

---
+++ 06_ex.js
@@ -0,0 +1,27 @@
+console.log('------------01--------------');
+
+var celsius = 25;
+
+var Fahrenheit = (9*celsius+160)/5;
+
+console.log(Fahrenheit+"°");
+
+console.log('------------02--------------');
+
+var grausFahrenheit = 77;
+
+var celsius1 = (grausFahrenheit-32)*(5/9);
+
+console.log(celsius1+"°");
+
+console.log('------------03--------------');
+
+var a = 3;
+
+var b = 7;
+
+var c = 9;
+
+var soma = (a**2)+(b**2)+(c**2);
+
+console.log(soma);
